7c06c958c0e93329bb4b5109421d89993d832c58
Ousmane Dia <56128706+UsmanJah@users.noreply.github.com>
2021-03-18 22:46:14 +0000

    Add files via upload

---
+++ Linéarisation de fonctions mathématiques avec MatLab/Fonction1.m
@@ -0,0 +1,22 @@
+clc;
+close all;
+syms x;
+f= x^3-5*x^2+7*x-3; 
+g=diff(f); 
+n=input('Entrer la valeur de la partie décimale :');
+epsilon = 5*10^-(n+1);
+x0 = input('Entrer la valeur de la racine approximation :');
+for i=1:100
+     f0=vpa(subs(f,x,x0));
+     f0_der=vpa(subs(g,x,x0));
+  y=x0-f0/f0_der; 
+err=abs(y-x0);
+if err<epsilon 
+break
+end
+x0=y;
+end
+y = y - rem(y,10^-n);
+fprintf('La racine est  : %f \n',y);
+fprintf('Le nombre d’itération est de: %d\n',i);
+
